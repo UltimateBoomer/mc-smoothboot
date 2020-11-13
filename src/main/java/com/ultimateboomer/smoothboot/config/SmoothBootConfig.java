4e676fb7d691044fdbca91d2fb4f06a19e6646b2
UltimateBoomer <caohc2017@gmail.com>
2020-11-13 12:24:57 -0500

    1.15 first version

--- src/main/java/com/ultimateboomer/smoothboot/config/SmoothBootConfig.java
+++ src/main/java/com/ultimateboomer/smoothboot/config/SmoothBootConfig.java
@@ -10,21 +10,10 @@ import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry.Gui.Tooltip;
 @Config(name = SmoothBoot.MOD_ID)
 public class SmoothBootConfig implements ConfigData {
 	@Tooltip
-	public int bootstrapThreadCount = 1;
-	
-	@Tooltip
-	public int mainThreadCount = Runtime.getRuntime().availableProcessors() - 1;
+	public int serverThreadCount = Runtime.getRuntime().availableProcessors() - 1;
 	
 	@BoundedDiscrete(min = 1, max = 10)
 	@Tooltip
-	public int bootstrapPriority = 1;
-	
-	@BoundedDiscrete(min = 1, max = 10)
-	@Tooltip
-	public int mainPriority = 1;
-	
-	@BoundedDiscrete(min = 1, max = 10)
-	@Tooltip
-	public int ioPriority = 5;
+	public int serverPriority = 1;
 }
  
--- src/main/java/com/ultimateboomer/smoothboot/mixin/UtilMixin.java
+++ src/main/java/com/ultimateboomer/smoothboot/mixin/UtilMixin.java
@@ -1,9 +1,8 @@
 package com.ultimateboomer.smoothboot.mixin;
 
-import java.util.Objects;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinWorkerThread;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -17,66 +16,40 @@ import org.spongepowered.asm.mixin.Shadow;
 import com.ultimateboomer.smoothboot.LoggingForkJoinWorkerThread;
 import com.ultimateboomer.smoothboot.SmoothBoot;
 
+import net.minecraft.Bootstrap;
 import net.minecraft.util.Util;
+import net.minecraft.util.crash.CrashException;
 import net.minecraft.util.math.MathHelper;
 
 @Mixin(Util.class)
 public abstract class UtilMixin {
 	private static boolean initConfig = false;
-	private static boolean initBootstrap = false;
-	private static boolean initMainWorker = false;
-	private static boolean initIOWorker = false;
+	private static boolean initServerWorker = false;
 	
 	@Shadow
-	private static ExecutorService BOOTSTRAP_EXECUTOR;
-	
-	@Shadow
-	private static ExecutorService MAIN_WORKER_EXECUTOR;
-	
-	@Shadow
-	private static ExecutorService IO_WORKER_EXECUTOR;
+	private static ExecutorService SERVER_WORKER_EXECUTOR;
 	
 	@Shadow
 	@Final
-	private static AtomicInteger NEXT_WORKER_ID;
+	private static AtomicInteger NEXT_SERVER_WORKER_ID;
 	
 	@Shadow
 	@Final
 	private static Logger LOGGER;
 	
 	@Shadow
-	protected static void method_18347(Thread thread, Throwable throwable) {};
+	protected static <T extends Throwable> T throwOrPause(T t) { return null; };
 	
 	// Probably not ideal, but this is the only way I found to modify createWorker without causing errors.
 	// Redirecting or overwriting causes static initialization to be called too early resulting in NullPointerException being thrown.
 	@Overwrite()
-	public static Executor getBootstrapExecutor() {
-		if (!initBootstrap) {
-			BOOTSTRAP_EXECUTOR = replWorker("Bootstrap");
-			SmoothBoot.LOGGER.debug("Bootstrap worker replaced");
-			initBootstrap = true;
-		}
-		return BOOTSTRAP_EXECUTOR;
-	}
-	
-	@Overwrite()
-	public static Executor getMainWorkerExecutor() {
-		if (!initMainWorker) {
-			MAIN_WORKER_EXECUTOR = replWorker("Main");
+	public static Executor getServerWorkerExecutor() {
+		if (!initServerWorker) {
+			SERVER_WORKER_EXECUTOR = replWorker("Main");
 			SmoothBoot.LOGGER.debug("Main worker replaced");
-			initMainWorker = true;
+			initServerWorker = true;
 		}
-		return MAIN_WORKER_EXECUTOR;
-	}
-	
-	@Overwrite
-	public static Executor getIoWorkerExecutor() {
-		if (!initIOWorker) {
-			IO_WORKER_EXECUTOR = replIoWorker();
-			SmoothBoot.LOGGER.debug("IO worker replaced");
-			initIOWorker = true;
-		}
-		return IO_WORKER_EXECUTOR;
+		return SERVER_WORKER_EXECUTOR;
 	}
 	
 	// Replace createWorker
@@ -86,36 +59,28 @@ public abstract class UtilMixin {
 			initConfig = true;
 		}
 		
-		ExecutorService executorService2 = new ForkJoinPool(MathHelper.clamp(select(name, SmoothBoot.config.bootstrapThreadCount,
-			SmoothBoot.config.mainThreadCount), 1, 0x7fff), (forkJoinPool) -> {
-				String workerName = "Worker-" + name + "-" + NEXT_WORKER_ID.getAndIncrement();
+		ExecutorService executorService2 = new ForkJoinPool(MathHelper.clamp(SmoothBoot.config.serverThreadCount,
+			1, 0x7fff), (forkJoinPool) -> {
+				String workerName = "Server-Worker-" + name + "-" + NEXT_SERVER_WORKER_ID.getAndIncrement();
 				SmoothBoot.LOGGER.debug("Initialized " + workerName);
 				
 				ForkJoinWorkerThread forkJoinWorkerThread = new LoggingForkJoinWorkerThread(forkJoinPool, LOGGER);
-				forkJoinWorkerThread.setPriority(select(name, SmoothBoot.config.bootstrapPriority, SmoothBoot.config.mainPriority));
+				forkJoinWorkerThread.setPriority(SmoothBoot.config.serverPriority);
 				forkJoinWorkerThread.setName(workerName);
 				return forkJoinWorkerThread;
-		}, UtilMixin::method_18347, true);
-		SmoothBoot.LOGGER.info(executorService2);
+		}, (thread, throwable) -> {
+            throwOrPause(throwable);
+            if (throwable instanceof CompletionException) {
+               throwable = throwable.getCause();
+            }
+            
+            if (throwable instanceof CrashException) {
+               Bootstrap.println(((CrashException)throwable).getReport().asString());
+               System.exit(-1);
+            }
+            
+            LOGGER.error(String.format("Caught exception in thread %s", thread), throwable);
+         }, true);
 		return executorService2;
-	}
-	
-	// Replace createIoWorker
-	private static ExecutorService replIoWorker() {
-		return Executors.newCachedThreadPool((runnable) -> {
-			String workerName = "IO-Worker-" + NEXT_WORKER_ID.getAndIncrement();
-			SmoothBoot.LOGGER.debug("Initialized " + workerName);
-			
-			Thread thread = new Thread(runnable);
-			thread.setName(workerName);
-			thread.setDaemon(true);
-			thread.setPriority(SmoothBoot.config.ioPriority);
-			thread.setUncaughtExceptionHandler(UtilMixin::method_18347);
-			return thread;
-		});
-	}
-	
-	private static <T> T select(String name, T bootstrap, T main) {
-		return Objects.equals(name, "Bootstrap") ? bootstrap : main;
 	}
 }
