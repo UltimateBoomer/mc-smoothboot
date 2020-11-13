7409d602f278938db2a50a16f6315681126a3ecd
UltimateBoomer <caohc2017@gmail.com>
2020-11-13 13:14:51 -0500

    Moved LoggingForkJoinWorkerThread, reorganized mod versioning in build.gradle

--- src/main/java/com/ultimateboomer/smoothboot/mixin/UtilMixin.java
+++ src/main/java/com/ultimateboomer/smoothboot/mixin/UtilMixin.java
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import com.ultimateboomer.smoothboot.LoggingForkJoinWorkerThread;
 import com.ultimateboomer.smoothboot.SmoothBoot;
+import com.ultimateboomer.smoothboot.util.LoggingForkJoinWorkerThread;
 
 import net.minecraft.Bootstrap;
 import net.minecraft.util.Util;
