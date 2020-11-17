d3bb4638e0c50961f1a9fdbabb2bbca4a222d624
Steven Cao <caohc2017@gmail.com>
2020-11-16 22:31:05 -0500

    Update to 1.3.0, restructure config

--- src/main/java/com/ultimateboomer/smoothboot/config/SmoothBootConfig.java
+++ src/main/java/com/ultimateboomer/smoothboot/config/SmoothBootConfig.java
@@ -5,30 +5,26 @@ import com.ultimateboomer.smoothboot.SmoothBoot;
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry.BoundedDiscrete;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry.Gui.Tooltip;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry.Gui.PrefixText;
 
 @Config(name = SmoothBoot.MOD_ID)
 public class SmoothBootConfig implements ConfigData {
-	@Tooltip
+	@PrefixText
 	public int bootstrapThreadCount = 1;
 	
-	@Tooltip
 	public int mainThreadCount = Runtime.getRuntime().availableProcessors() - 1;
 	
+	@PrefixText
 	@BoundedDiscrete(min = 1, max = 10)
-	@Tooltip
 	public int gamePriority = 5;
 	
 	@BoundedDiscrete(min = 1, max = 10)
-	@Tooltip
 	public int bootstrapPriority = 1;
 	
 	@BoundedDiscrete(min = 1, max = 10)
-	@Tooltip
 	public int mainPriority = 1;
 	
 	@BoundedDiscrete(min = 1, max = 10)
-	@Tooltip
 	public int ioPriority = 5;
 }
  
