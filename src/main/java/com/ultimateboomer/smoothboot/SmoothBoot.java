7308077f2f188583b6ec7178b8149f106104c1ce
Steven Cao <caohc2017@gmail.com>
2020-11-19 16:39:07 -0500

    Add MixinBootstrap check

--- src/main/java/com/ultimateboomer/smoothboot/SmoothBoot.java
+++ src/main/java/com/ultimateboomer/smoothboot/SmoothBoot.java
@@ -3,6 +3,8 @@ package com.ultimateboomer.smoothboot;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.ultimateboomer.smoothboot.config.SmoothBootConfigHandler;
+
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(SmoothBoot.MOD_ID)
@@ -13,6 +15,9 @@ public class SmoothBoot {
 	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
 	public SmoothBoot() {
-		
+		// Check if mixin is loaded, config must be initalized by this point if mixin works
+		if (SmoothBootConfigHandler.config == null) {
+			throw new IllegalStateException("Mixin not loaded! Make sure MixinBootstrap is installed.");
+		}
 	}
 }
