e7dc7943028a60509957bde27c8fc673eeaaa602
Steven Cao <caohc2017@gmail.com>
2020-11-24 17:25:56 -0500

    Group config settings into classes, change config default values, add more description to config

--- src/main/java/com/ultimateboomer/smoothboot/mixin/server/MinecraftServerMixin.java
+++ src/main/java/com/ultimateboomer/smoothboot/mixin/server/MinecraftServerMixin.java
@@ -19,7 +19,7 @@ public class MinecraftServerMixin {
 			SmoothBootState.initConfig = true;
 		}
 		
-		Thread.currentThread().setPriority(SmoothBoot.config.gamePriority);
+		Thread.currentThread().setPriority(SmoothBoot.config.threadPriority.game);
 		SmoothBoot.LOGGER.debug("Initialized server game thread");
 	}
 }
