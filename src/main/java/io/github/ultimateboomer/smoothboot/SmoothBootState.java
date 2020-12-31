6dfdbea5b83e9df4e6905ad79446011318b2b4dc
Steven Cao <caohc2017@gmail.com>
2020-12-31 17:28:39 -0500

    Add datafixers build optimization, bump version, configure bintray

--- src/main/java/io/github/ultimateboomer/smoothboot/SmoothBootState.java
+++ src/main/java/io/github/ultimateboomer/smoothboot/SmoothBootState.java
@@ -1,8 +1,14 @@
 package io.github.ultimateboomer.smoothboot;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class SmoothBootState {
 	public static boolean initConfig = false;
 	public static boolean initServerWorker = false;
 	public static boolean initIOWorker = false;
 	public static boolean gameLoaded = false;
+
+	public static final ExecutorService SINGLE_THREADED_EXECUTOR = Executors.newSingleThreadExecutor();
+
 }
