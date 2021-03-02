477e58942409291221d6c10304338c836dd9f975
Steven Cao <caohc2017@gmail.com>
2021-03-02 13:21:05 -0500

    Fix "Incompatible Mod List" message

--- src/main/java/io/github/ultimateboomer/smoothboot/SmoothBoot.java
+++ src/main/java/io/github/ultimateboomer/smoothboot/SmoothBoot.java
@@ -1,9 +1,12 @@
 package io.github.ultimateboomer.smoothboot;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import net.minecraftforge.fml.common.Mod;
 
 @Mod(SmoothBoot.MOD_ID)
 public class SmoothBoot {
@@ -11,4 +14,9 @@ public class SmoothBoot {
 	public static final String MOD_NAME = "Smooth Boot";
 	
 	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+
+	public SmoothBoot() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+				() -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+	}
 }
