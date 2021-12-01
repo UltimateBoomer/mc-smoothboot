9b92a8ca47828a43054169dac3692cb37aa63f9f
fanta <tobiasmuellner@outlook.com>
2021-12-01 22:48:10 +0100

    Update to 1.18

--- src/main/java/io/github/ultimateboomer/smoothboot/mixin/UtilMixin.java
+++ src/main/java/io/github/ultimateboomer/smoothboot/mixin/UtilMixin.java
@@ -6,10 +6,7 @@ import io.github.ultimateboomer.smoothboot.util.LoggingForkJoinWorkerThread;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.MathHelper;
 import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 
 import java.util.Objects;
 import java.util.concurrent.*;
@@ -17,12 +14,18 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Mixin(Util.class)
 public abstract class UtilMixin {
+	@Mutable
+	@Final
 	@Shadow
 	private static ExecutorService BOOTSTRAP_EXECUTOR;
 	
+	@Mutable
+	@Final
 	@Shadow
 	private static ExecutorService MAIN_WORKER_EXECUTOR;
 	
+	@Mutable
+	@Final
 	@Shadow
 	private static ExecutorService IO_WORKER_EXECUTOR;
 	
@@ -32,10 +35,10 @@ public abstract class UtilMixin {
 	
 	@Shadow
 	@Final
-	private static Logger LOGGER;
+	static Logger LOGGER;
 	
 	@Shadow
-	protected static void method_18347(Thread thread, Throwable throwable) {};
+	private static void uncaughtExceptionHandler(Thread thread, Throwable throwable) {};
 	
 	// Probably not ideal, but this is the only way I found to modify createWorker without causing errors.
 	// Redirecting or overwriting causes static initialization to be called too early resulting in NullPointerException being thrown.
@@ -45,7 +48,7 @@ public abstract class UtilMixin {
 	 * @author UltimateBoomer
 	 */
 	@Overwrite
-	public static Executor getBootstrapExecutor() {
+	public static ExecutorService getBootstrapExecutor() {
 		if (!SmoothBootState.initBootstrap) {
 			BOOTSTRAP_EXECUTOR = replWorker("Bootstrap");
 			SmoothBoot.LOGGER.debug("Bootstrap worker replaced");
@@ -58,7 +61,7 @@ public abstract class UtilMixin {
 	 * @author UltimateBoomer
 	 */
 	@Overwrite
-	public static Executor getMainWorkerExecutor() {
+	public static ExecutorService getMainWorkerExecutor() {
 		if (!SmoothBootState.initMainWorker) {
 			MAIN_WORKER_EXECUTOR = replWorker("Main");
 			SmoothBoot.LOGGER.debug("Main worker replaced");
@@ -71,7 +74,7 @@ public abstract class UtilMixin {
 	 * @author UltimateBoomer
 	 */
 	@Overwrite
-	public static Executor getIoWorkerExecutor() {
+	public static ExecutorService getIoWorkerExecutor() {
 		if (!SmoothBootState.initIOWorker) {
 			IO_WORKER_EXECUTOR = replIoWorker();
 			SmoothBoot.LOGGER.debug("IO worker replaced");
@@ -81,30 +84,29 @@ public abstract class UtilMixin {
 	}
 
 	/**
-	 * Replace {@link Util#createWorker}
+	 * Replace
 	 */
 	private static ExecutorService replWorker(String name) {
 		if (!SmoothBootState.initConfig) {
 			SmoothBoot.regConfig();
 			SmoothBootState.initConfig = true;
 		}
-		
-		ExecutorService executorService2 = new ForkJoinPool(MathHelper.clamp(select(name, SmoothBoot.config.threadCount.bootstrap,
+
+		return new ForkJoinPool(MathHelper.clamp(select(name, SmoothBoot.config.threadCount.bootstrap,
 			SmoothBoot.config.threadCount.main), 1, 0x7fff), (forkJoinPool) -> {
 				String workerName = "Worker-" + name + "-" + NEXT_WORKER_ID.getAndIncrement();
 				SmoothBoot.LOGGER.debug("Initialized " + workerName);
-				
+
 				ForkJoinWorkerThread forkJoinWorkerThread = new LoggingForkJoinWorkerThread(forkJoinPool, LOGGER);
 				forkJoinWorkerThread.setPriority(select(name, SmoothBoot.config.threadPriority.bootstrap,
 					SmoothBoot.config.threadPriority.main));
 				forkJoinWorkerThread.setName(workerName);
 				return forkJoinWorkerThread;
-		}, UtilMixin::method_18347, true);
-		return executorService2;
+		}, UtilMixin::uncaughtExceptionHandler, true);
 	}
 
 	/**
-	 * Replace {@link Util#createIoWorker}
+	 * Replace
 	 */
 	private static ExecutorService replIoWorker() {
 		return Executors.newCachedThreadPool((runnable) -> {
@@ -115,7 +117,7 @@ public abstract class UtilMixin {
 			thread.setName(workerName);
 			thread.setDaemon(true);
 			thread.setPriority(SmoothBoot.config.threadPriority.io);
-			thread.setUncaughtExceptionHandler(UtilMixin::method_18347);
+			thread.setUncaughtExceptionHandler(UtilMixin::uncaughtExceptionHandler);
 			return thread;
 		});
 	}
