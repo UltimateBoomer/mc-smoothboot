cbb5f1097e25bc487b2e764e95ff6098877e5ce1
fanta <tobiasmuellner@outlook.com>
2022-05-28 07:24:15 +0200

    Update 1.19-pre3

--- src/main/java/io/github/ultimateboomer/smoothboot/mixin/client/IntegratedServerMixin.java
+++ src/main/java/io/github/ultimateboomer/smoothboot/mixin/client/IntegratedServerMixin.java
@@ -1,14 +1,12 @@
 package io.github.ultimateboomer.smoothboot.mixin.client;
 
-import com.mojang.authlib.GameProfileRepository;
-import com.mojang.authlib.minecraft.MinecraftSessionService;
 import io.github.ultimateboomer.smoothboot.SmoothBoot;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.SaveLoader;
 import net.minecraft.server.WorldGenerationProgressListenerFactory;
 import net.minecraft.server.integrated.IntegratedServer;
-import net.minecraft.util.UserCache;
+import net.minecraft.util.ApiServices;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(IntegratedServer.class)
 public class IntegratedServerMixin {
     @Inject(method = "<init>", at = @At("RETURN"))
-    public void onInit(Thread serverThread, MinecraftClient client, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, MinecraftSessionService sessionService, GameProfileRepository gameProfileRepo, UserCache userCache, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
+    public void onInit(Thread serverThread, MinecraftClient client, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, ApiServices apiServices, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
         serverThread.setPriority(SmoothBoot.config.threadPriority.integratedServer);
         SmoothBoot.LOGGER.debug("Initialized integrated server thread");
     }
